08d2c34bbd326ab70606c66d9026716ff350e5f2
MohamedAlyEtriby <mohamedalyetriby@gmail.com>
2023-09-12 13:30:16 +0300

    Slider

---
+++ src/Slider.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { FcNext } from "react-icons/fc";
+import { FcPrevious } from "react-icons/fc";
+import slides from "./SliderData";
+
+const Slider = () => {
+  const [startIndex, setStartIndex] = useState(0);
+
+  const endIndex = (startIndex + 8) % slides.length;
+  const slicedItems =
+    endIndex > startIndex
+      ? slides.slice(startIndex, endIndex)
+      : [
+          ...slides.slice(startIndex, slides.length),
+          ...slides.slice(0, endIndex),
+        ];
+
+  const handleClickNext = () => {
+    setStartIndex((startIndex + 1) % slides.length);
+  };
+  const handleClickPrev = () => {
+    setStartIndex(startIndex - 1 < 0 ? slides.length - 1 : startIndex - 1);
+  };
+  return (
+    <div className="sliderclicks">
+      <div className="slider">
+        {slicedItems.map((item, index) => {
+          return (
+            <div className={`rot ${index === 6 && "current"} item`}>
+              <img src={item.image} alt="" className="sora" />
+            </div>
+          );
+        })}
+      </div>
+      <div className="controlSlider">
+        <button className="Button" onClick={handleClickPrev}>
+          <FcPrevious size={40} />
+        </button>
+        <div>
+          <p>{slicedItems[7].Name}</p>
+          <p>{slicedItems[7].title}</p>
+        </div>
+        <button onClick={handleClickNext} className="Button">
+          <FcNext size={40} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
---
+++ src/SliderData.js
@@ -0,0 +1,66 @@
+const slides = [
+  {
+    image: "/assets/Asset1.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    image: "/assets/Asset2.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    image: "/assets/Asset3.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    image: "/assets/Asset4.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    image: "/assets/Asset5.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    image: "/assets/Asset6.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    image: "/assets/Asset3.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    image: "/assets/Asset4.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+  {
+    image: "/assets/Asset5.png",
+    alt: "Slide 1",
+    title:
+      "Lorem ipsum dolor sit amet,consectetuer adipiscing elit, sed diam nonummy nibh euismo  tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim",
+    Name: "Project Name",
+  },
+];
+export default slides;
