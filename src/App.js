0f942d47ba27de5d551e7291b91b24b8ec0a8ca3
MohamedAlyEtriby <mohamedalyetriby@gmail.com>
2023-09-13 13:07:24 +0300

    index

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Slider from "./components/Slider";
-import { useState } from "react";
 
 function App() {
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  const handleClick = () => {
-    setIsExpanded(!isExpanded);
-  };
-
+ 
   return (
     <div className="App">
       <Slider />
-      <div>
-      <div className={`box ${isExpanded ? 'expanded' : ''}`}></div>
-      <button onClick={handleClick}>Next</button>
-    </div>
+    
     </div>
   );
 }
